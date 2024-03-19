66a72fa67ebf5a23adaec5cc3e38549dbe5daf23
Felix Parker <flixpar@gmail.com>
2024-03-19 00:03:35 -0400

    add config path arg

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
+import sys
 import toml
 
 from tasks import get_trainer
 from utils import *
 
 
-def main():
-    config = toml.load("config.toml")
+def main(config_path):
+    config = toml.load(config_path)
     config = dict_to_object(config)
 
     run_id = get_run_id(config.DEBUG)
     trainer = get_trainer(run_id, config)
 
     trainer.train()
+    trainer.test()
 
 if __name__ == "__main__":
-    main()
+    config_path = sys.argv[1] if len(sys.argv) > 1 else "configs/config.toml"
+    main(config_path)
