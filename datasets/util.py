e039eb0a982eb6c722f3cb0b472102516f725a02
Felix Parker <flixpar@gmail.com>
2024-03-19 11:37:04 -0400

    fixed multi2uni dataset

--- datasets/util.py
+++ datasets/util.py
@@ -4,12 +4,13 @@ from torch.utils.data import Dataset
 class Multi2UniDataset(Dataset):
     def __init__(self, dataset):
         self.dataset = dataset
-        self.n_features = 1
         self.__dict__.update(dataset.__dict__)
+        self.real_features = dataset.n_features
+        self.n_features = 1
 
     def __getitem__(self, index):
-        example_idx = index // self.n_features
-        feature_idx = index % self.n_features
+        example_idx = index // self.real_features
+        feature_idx = index % self.real_features
 
         if self.task == "forecasting":
             x_enc, x_dec, y = self.dataset[example_idx]
@@ -21,10 +22,10 @@ class Multi2UniDataset(Dataset):
             return x_enc, x_dec, label
 
     def __len__(self):
-        return len(self.dataset) * self.n_features
-    
+        return len(self.dataset) * self.real_features
+
     def inverse_index(self, index):
-        example_idx = index // self.n_features
-        feature_idx = index % self.n_features
+        example_idx = self.dataset.inverse_index(index // self.real_features)
+        feature_idx = index % self.real_features
         return example_idx, feature_idx
 
