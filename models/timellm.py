c91da9e164b292a7238fb904841e9932d998e1dc
Felix Parker <flixpar@gmail.com>
2024-03-22 13:58:54 -0400

    add covariates to timellm

--- models/timellm.py
+++ models/timellm.py
@@ -17,8 +17,8 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 class TimeLLM(nn.Module):
 
-    supported_tasks = ["forecasting", "anomaly_detection", "semantic_segmentation"]
-    supported_modes = ["univariate"]
+    supported_tasks = ["forecasting", "anomaly_detection", "semantic_segmentation", "segmentation"]
+    supported_modes = ["univariate", "multivariate"]
 
     def __init__(self, config, dataset):
         super(TimeLLM, self).__init__()
@@ -27,6 +27,10 @@ class TimeLLM(nn.Module):
 
         self.pred_len = self.config.pred_len
         self.seq_len = self.config.history_len
+
+        self.task = self.config.task
+        self.task_description = self.get_task_description()
+        self.dataset_description = dataset.description
 
         self.d_ff = self.model_config.d_ff
         self.d_model = self.model_config.d_model
@@ -38,18 +42,45 @@ class TimeLLM(nn.Module):
         self.patch_len = self.model_config.patching.patch_len
         self.stride = self.model_config.patching.stride
         self.n_patches = int((self.seq_len - self.patch_len) / self.stride + 2)
+        self.d_patch = self.d_model
 
-        self.task = self.config.task
-        self.task_description = self.get_task_description()
-        self.dataset_description = dataset.description
+        self.covariate_mode = self.model_config.covariate_mode
+        self.n_features = dataset.n_features
+
+        if self.task in ["forecasting", "anomaly_detection"]:
+            self.n_outputs_per_step = self.n_features
+        elif self.task == "semantic_segmentation":
+            if dataset.n_classes == 2:
+                self.n_outputs_per_step = 1
+            else:
+                self.n_outputs_per_step = dataset.n_classes
+        elif self.task == "segmentation":
+            self.n_outputs_per_step = 1
+        else:
+            raise ValueError(f"Task {self.task} is not supported.")
+        self.n_outputs = self.n_outputs_per_step * self.pred_len
+
+        match self.covariate_mode:
+            case "univariate":
+                assert self.n_features == 1
+            case "interleave":
+                self.n_patches *= self.n_features
+            case "concat":
+                self.d_model *= self.n_features
+            case "independent":
+                self.n_outputs = self.pred_len
+            case "add":
+                pass
+            case _:
+                raise ValueError(f"Unknown covariate mode {self.covariate_mode}")
 
         self.setup_llm()
 
-        self.normalize_layers = RevIN(dataset.n_features, affine=False)
+        self.normalize_layers = RevIN(self.n_features, affine=False)
         self.mapping_layer = nn.Linear(self.vocab_size, self.num_tokens)
-        self.patch_embedding = PatchEmbedding(self.d_model, self.patch_len, self.stride, self.dropout, pos_embed=False)
+        self.patch_embedding = PatchEmbedding(self.d_patch, self.patch_len, self.stride, self.dropout, pos_embed=False)
         self.reprogramming_layer = ReprogrammingLayer(self.d_model, self.n_attention_heads, self.d_ff, self.d_llm, attention_dropout=self.dropout)
-        self.output_projection = FlattenHead(dataset.n_features, self.d_ff * self.n_patches, self.pred_len, head_dropout=0)
+        self.output_projection = FlattenHead(self.d_ff * self.n_patches, self.n_outputs, head_dropout=0)
 
         if not self.llm_enabled:
             self.llm_replacement = nn.Sequential(
@@ -118,86 +149,110 @@ class TimeLLM(nn.Module):
         return state_dict
 
     def forward(self, x_enc, x_dec):
-        forecast_fn = self.forecast if self.llm_enabled else self.forecast_nollm
         if self.task in ["forecasting", "anomaly_detection"]:
-            return forecast_fn(x_enc, x_dec)
-        if self.task == "semantic_segmentation":
-            pred = forecast_fn(x_enc, x_dec)
+            return self.forecast(x_enc, x_dec)
+        elif self.task == "semantic_segmentation":
+            pred = self.forecast(x_enc, x_dec)
             pred = F.sigmoid(pred)
             return pred
+        elif self.task == "segmentation":
+            if self.config.tasks.segmentation.mode == "boundary-prediction":
+                pred = self.forecast(x_enc, x_dec)
+                pred = F.sigmoid(pred)
+                return pred
+            elif self.config.tasks.segmentation.mode == "steps-to-boundary":
+                raise NotImplementedError("Steps-to-boundary segmentation not yet implemented for TimeLLM")
+            else:
+                raise ValueError(f"Segmentation mode {self.config.tasks.segmentation.mode} not implemented for TimeLLM")
         else:
             raise ValueError(f"Task {self.task_name} not implemented")
 
     def forecast(self, x_enc, x_dec=None):
 
-        x_enc = x_enc.unsqueeze(-1)
+        if x_enc.ndim == 2:
+            x_enc = x_enc.unsqueeze(-1)
+
+        bs, seq_len, n_features = x_enc.size()
+        assert n_features == self.n_features
+
+        if self.llm_enabled:
+            prompt = self.build_prompt(x_enc)
+
         x_enc = self.normalize_layers(x_enc, "norm")
 
+        x_enc = x_enc.permute(0, 2, 1).contiguous() # [bs, n_features, seq_len]
+        enc_out, _ = self.patch_embedding(x_enc)    # [bs * n_features, n_patches, d_patch]
+
+        if self.covariate_mode == "concat":
+            enc_out = enc_out.reshape(bs, n_features, self.n_patches, self.d_patch)
+            enc_out = enc_out.permute(0, 2, 1, 3) # [bs, n_patches, n_features, d_patch]
+            enc_out = enc_out.reshape(bs, self.n_patches, n_features * self.d_patch)
+
+        source_embeddings = self.mapping_layer(self.word_embeddings.permute(1, 0)).permute(1, 0)
+        enc_out = self.reprogramming_layer(enc_out, source_embeddings, source_embeddings) # [bs * n_features, n_patches, d_llm]
+
+        if self.covariate_mode == "add":
+            enc_out = enc_out.reshape(bs, n_features, self.n_patches, self.d_llm)
+            enc_out = enc_out.mean(dim=1)
+        elif self.covariate_mode == "interleave":
+            enc_out = enc_out.reshape(bs, n_features, -1, self.d_llm) # [bs, n_features, n_patches, d_llm]
+            enc_out = enc_out.permute(0, 2, 1, 3)                     # [bs, n_patches, n_features, d_llm]
+            enc_out = enc_out.reshape(bs, -1, self.d_llm)             # [bs, n_patches * n_features, d_llm]
+
+        if self.llm_enabled:
+            prompt = self.tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=2048).input_ids
+            prompt_embeddings = self.llm.get_input_embeddings()(prompt.to(x_enc.device)) # [bs, n_tok, d_llm]
+
+            if self.covariate_mode == "independent":
+                prompt_embeddings = prompt_embeddings.repeat_interleave(n_features, dim=0)
+
+            llm_enc_out = torch.cat([prompt_embeddings, enc_out], dim=1)
+            dec_out = self.llm(inputs_embeds=llm_enc_out).last_hidden_state  # [bs, n_tok + n_patches, d_llm]
+            dec_out = dec_out.to(x_enc.dtype)
+        else:
+            dec_out = self.llm_replacement(enc_out)
+
+        dec_out = dec_out[:, -self.n_patches:, :self.d_ff]
+        dec_out = dec_out.permute(0, 2, 1).contiguous() # [bs, d_ff, n_patches]
+        dec_out = self.output_projection(dec_out)       # [bs, pred_len * n_features]
+        dec_out = dec_out.view(bs, self.pred_len, self.n_outputs_per_step).squeeze(-1)
+
+        if self.task in ["forecasting", "anomaly_detection"]:
+            dec_out = self.normalize_layers(dec_out, "denorm")
+
+        return dec_out
+
+    def build_prompt(self, x_enc):
         with torch.no_grad():
-            min_values = torch.min(x_enc, dim=1)[0]
-            max_values = torch.max(x_enc, dim=1)[0]
+            min_values = torch.min(x_enc, dim=1).values
+            max_values = torch.max(x_enc, dim=1).values
             medians = torch.median(x_enc.float(), dim=1).values
             lags = calcute_lags(x_enc.float(), self.n_lags)
             trends = x_enc.diff(dim=1).sum(dim=1)
 
-        prompt = []
+        prompts = []
         for b in range(x_enc.size(0)):
-            min_values_str = str(min_values[b].tolist()[0])
-            max_values_str = str(max_values[b].tolist()[0])
-            median_values_str = str(medians[b].tolist()[0])
-            lags_values_str = str(lags[b].tolist())
-            prompt_ = (
-                f"<|start_prompt|>Dataset description: {self.dataset_description} "
+            min_value = min_values[b][0].item()
+            max_value = max_values[b][0].item()
+            median_value = medians[b][0].item()
+            lags_value = lags[b].tolist()
+            trend_dir = "upward" if trends[b].mean() > 0 else "downward"
+            prompt = (
+                f"<|start_prompt|>"
+                f"Dataset description: {self.dataset_description} "
                 f"Task description: {self.task_description} "
-                "Input statistics: "
-                f"min value {min_values_str}, "
-                f"max value {max_values_str}, "
-                f"median value {median_values_str}, "
-                f"the trend of input is {'upward' if trends[b] > 0 else 'downward'}, "
-                f"top 5 lags are : {lags_values_str}<|<end_prompt>|>"
+                f"Input statistics: "
+                f"min value = {min_value:.3f}, "
+                f"max value = {max_value:.3f}, "
+                f"median value = {median_value:.3f}, "
+                f"the trend of input is {trend_dir}, "
+                f"top 5 lags are {lags_value}"
+                f"<|<end_prompt>|>"
             )
 
-            prompt.append(prompt_)
+            prompts.append(prompt)
 
-        prompt = self.tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=2048).input_ids
-        prompt_embeddings = self.llm.get_input_embeddings()(prompt.to(x_enc.device))
-
-        x_enc = x_enc.permute(0, 2, 1).contiguous()
-        enc_out, _ = self.patch_embedding(x_enc)
-        source_embeddings = self.mapping_layer(self.word_embeddings.permute(1, 0)).permute(1, 0)
-        enc_out = self.reprogramming_layer(enc_out, source_embeddings, source_embeddings)
-
-        llm_enc_out = torch.cat([prompt_embeddings, enc_out], dim=1)
-        dec_out = self.llm(inputs_embeds=llm_enc_out).last_hidden_state
-        dec_out = dec_out.to(x_enc.dtype)
-
-        dec_out = dec_out[:, -self.n_patches:, :self.d_ff]
-        dec_out = dec_out.permute(0, 2, 1).contiguous()
-        dec_out = self.output_projection(dec_out)
-
-        dec_out = self.normalize_layers(dec_out.unsqueeze(-1), "denorm").squeeze(-1)
-
-        return dec_out
-
-    def forecast_nollm(self, x_enc, x_dec=None):
-        source_embeddings = self.mapping_layer(self.word_embeddings.permute(1, 0)).permute(1, 0)
-
-        x_enc = x_enc.unsqueeze(-1)
-        x_enc = self.normalize_layers(x_enc, "norm")
-
-        x_enc = x_enc.permute(0, 2, 1).contiguous()
-        enc_out, _ = self.patch_embedding(x_enc)
-        enc_out = self.reprogramming_layer(enc_out, source_embeddings, source_embeddings)
-
-        dec_out = self.llm_replacement(enc_out)
-
-        dec_out = dec_out[:, -self.n_patches:, :self.d_ff]
-        dec_out = dec_out.permute(0, 2, 1).contiguous()
-        dec_out = self.output_projection(dec_out)
-
-        dec_out = self.normalize_layers(dec_out.unsqueeze(-1), "denorm").squeeze(-1)
-
-        return dec_out
+        return prompts
 
     def get_task_description(self):
         if self.task == "forecasting":
@@ -206,9 +261,12 @@ class TimeLLM(nn.Module):
             self.task_description = f"Reconstruct the past {self.seq_len} steps of data as accurately as possible using the following information."
         elif self.task == "semantic_segmentation":
             self.task_description = f"Classify the past {self.seq_len} steps of data as accurately as possible using the following information."
+        elif self.task == "segmentation":
+            self.task_description = f"Identify the change points in the past {self.seq_len} steps of data to segment the sequence."
         else:
             raise ValueError(f"Task {self.task} is not supported.")
         return self.task_description
+
 
 def calcute_lags(x, n_lags=5):
     x = x.permute(0, 2, 1).contiguous()
@@ -222,9 +280,8 @@ def calcute_lags(x, n_lags=5):
 
 
 class FlattenHead(nn.Module):
-    def __init__(self, n_vars, nf, target_window, head_dropout=0):
+    def __init__(self, nf, target_window, head_dropout=0):
         super().__init__()
-        self.n_vars = n_vars
         self.flatten = nn.Flatten(start_dim=-2)
         self.linear = nn.Linear(nf, target_window)
         self.dropout = nn.Dropout(head_dropout)
