1ee66e5c681d8a7162ee5dba16ab7503e44e1cd2
Felix Parker <flixpar@gmail.com>
2024-03-25 15:18:58 -0400

    add patchtst

---
+++ models/PatchTST.py
@@ -0,0 +1,383 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from .layers.Transformer_EncDec import Encoder, EncoderLayer
+from .layers.SelfAttention_Family import FullAttention, AttentionLayer
+from .layers.embed import PositionalEmbedding
+
+
+class PatchTST(nn.Module):
+    """
+    Paper link: https://arxiv.org/pdf/2211.14730.pdf
+    """
+
+    supported_tasks = [
+        "forecasting",
+        "anomaly_detection",
+        "imputation",
+        "classification",
+        "semantic_segmentation",
+        "segmentation",
+    ]
+    supported_modes = ["multivariate"]
+
+    def __init__(self, config, dataset):
+        """
+        patch_len: int, patch len for patch_embedding
+        stride: int, stride for patch_embedding
+        """
+        super().__init__()
+        self.config = config
+        self.model_config = self.config.models.patchtst
+
+        assert (
+            self.model_config.covariate_mode == "independent"
+        ), "Only independent covariate mode is supported for PatchTST"
+
+        self.task_name = self.config.task
+        self.seq_len = self.config.history_len
+        self.pred_len = self.config.pred_len
+
+        self.enc_in = dataset.n_features
+        self.num_class = (
+            dataset.n_classes
+            if self.task_name in ["classification", "semantic_segmentation"]
+            else 0
+        )
+
+        self.patch_len = self.model_config.patching.patch_len
+        self.stride = self.model_config.patching.stride
+        padding = self.stride
+
+        self.dropout = self.config.training.dropout
+        self.activation = "gelu"
+        self.output_attention = True
+        self.factor = 3
+
+        # patching and embedding
+        self.patch_embedding = PatchEmbedding(
+            self.model_config.d_model,
+            self.patch_len,
+            self.stride,
+            padding,
+            self.dropout,
+        )
+
+        # Encoder
+        self.encoder = Encoder(
+            [
+                EncoderLayer(
+                    AttentionLayer(
+                        FullAttention(
+                            False,
+                            self.factor,
+                            attention_dropout=self.dropout,
+                            output_attention=self.output_attention,
+                        ),
+                        self.model_config.d_model,
+                        self.model_config.n_heads,
+                    ),
+                    self.model_config.d_model,
+                    self.model_config.d_ff,
+                    dropout=self.dropout,
+                    activation=self.activation,
+                )
+                for l in range(self.model_config.e_layers)
+            ],
+            norm_layer=torch.nn.LayerNorm(self.model_config.d_model),
+        )
+
+        # Prediction Head
+        self.head_nf = self.model_config.d_model * int(
+            (self.seq_len - self.patch_len) / self.stride + 2
+        )
+        if self.task_name == "forecasting":
+            self.head = FlattenHead(
+                self.enc_in,
+                self.head_nf,
+                self.pred_len,
+                head_dropout=self.dropout,
+            )
+        elif self.task_name == "imputation" or self.task_name == "anomaly_detection":
+            self.head = FlattenHead(
+                self.enc_in,
+                self.head_nf,
+                self.seq_len,
+                head_dropout=self.dropout,
+            )
+        elif self.task_name == "classification":
+            self.flatten = nn.Flatten(start_dim=-2)
+            self.dropout = nn.Dropout(self.dropout)
+            self.projection = nn.Linear(self.head_nf * self.enc_in, self.num_class)
+        elif self.task_name == "semantic_segmentation":
+            n_out = self.num_class if self.num_class > 2 else 1
+            self.projection = nn.Linear(
+                self.head_nf * self.enc_in, self.pred_len * n_out
+            )
+        elif self.task_name == "segmentation":
+            self.projection = nn.Linear(self.head_nf * self.enc_in, self.pred_len)
+
+    def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
+        # Normalization from Non-stationary Transformer
+        means = x_enc.mean(1, keepdim=True).detach()
+        x_enc = x_enc - means
+        stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+        x_enc /= stdev
+
+        # do patching and embedding
+        x_enc = x_enc.permute(0, 2, 1)
+        # u: [bs * nvars x patch_num x d_model]
+        enc_out, n_vars = self.patch_embedding(x_enc)
+
+        # Encoder
+        # z: [bs * nvars x patch_num x d_model]
+        enc_out, attns = self.encoder(enc_out)
+        # z: [bs x nvars x patch_num x d_model]
+        enc_out = torch.reshape(
+            enc_out, (-1, n_vars, enc_out.shape[-2], enc_out.shape[-1])
+        )
+        # z: [bs x nvars x d_model x patch_num]
+        enc_out = enc_out.permute(0, 1, 3, 2)
+
+        # Decoder
+        dec_out = self.head(enc_out)  # z: [bs x nvars x target_window]
+        dec_out = dec_out.permute(0, 2, 1)
+
+        # De-Normalization from Non-stationary Transformer
+        dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+        dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.pred_len, 1))
+        return dec_out
+
+    def imputation(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask):
+        # Normalization from Non-stationary Transformer
+        means = torch.sum(x_enc, dim=1) / torch.sum(mask == 1, dim=1)
+        means = means.unsqueeze(1).detach()
+        x_enc = x_enc - means
+        x_enc = x_enc.masked_fill(mask == 0, 0)
+        stdev = torch.sqrt(
+            torch.sum(x_enc * x_enc, dim=1) / torch.sum(mask == 1, dim=1) + 1e-5
+        )
+        stdev = stdev.unsqueeze(1).detach()
+        x_enc /= stdev
+
+        # do patching and embedding
+        x_enc = x_enc.permute(0, 2, 1)
+        # u: [bs * nvars x patch_num x d_model]
+        enc_out, n_vars = self.patch_embedding(x_enc)
+
+        # Encoder
+        # z: [bs * nvars x patch_num x d_model]
+        enc_out, attns = self.encoder(enc_out)
+        # z: [bs x nvars x patch_num x d_model]
+        enc_out = torch.reshape(
+            enc_out, (-1, n_vars, enc_out.shape[-2], enc_out.shape[-1])
+        )
+        # z: [bs x nvars x d_model x patch_num]
+        enc_out = enc_out.permute(0, 1, 3, 2)
+
+        # Decoder
+        dec_out = self.head(enc_out)  # z: [bs x nvars x target_window]
+        dec_out = dec_out.permute(0, 2, 1)
+
+        # De-Normalization from Non-stationary Transformer
+        dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.seq_len, 1))
+        dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.seq_len, 1))
+        return dec_out
+
+    def anomaly_detection(self, x_enc):
+        # Normalization from Non-stationary Transformer
+        means = x_enc.mean(1, keepdim=True).detach()
+        x_enc = x_enc - means
+        stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+        x_enc /= stdev
+
+        # do patching and embedding
+        x_enc = x_enc.permute(0, 2, 1)
+        # u: [bs * nvars x patch_num x d_model]
+        enc_out, n_vars = self.patch_embedding(x_enc)
+
+        # Encoder
+        # z: [bs * nvars x patch_num x d_model]
+        enc_out, attns = self.encoder(enc_out)
+        # z: [bs x nvars x patch_num x d_model]
+        enc_out = torch.reshape(
+            enc_out, (-1, n_vars, enc_out.shape[-2], enc_out.shape[-1])
+        )
+        # z: [bs x nvars x d_model x patch_num]
+        enc_out = enc_out.permute(0, 1, 3, 2)
+
+        # Decoder
+        dec_out = self.head(enc_out)  # z: [bs x nvars x target_window]
+        dec_out = dec_out.permute(0, 2, 1)
+
+        # De-Normalization from Non-stationary Transformer
+        dec_out = dec_out * (stdev[:, 0, :].unsqueeze(1).repeat(1, self.seq_len, 1))
+        dec_out = dec_out + (means[:, 0, :].unsqueeze(1).repeat(1, self.seq_len, 1))
+        return dec_out
+
+    def classification(self, x_enc, x_mark_enc):
+        # Normalization from Non-stationary Transformer
+        means = x_enc.mean(1, keepdim=True).detach()
+        x_enc = x_enc - means
+        stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+        x_enc /= stdev
+
+        # do patching and embedding
+        x_enc = x_enc.permute(0, 2, 1)
+        # u: [bs * nvars x patch_num x d_model]
+        enc_out, n_vars = self.patch_embedding(x_enc)
+
+        # Encoder
+        # z: [bs * nvars x patch_num x d_model]
+        enc_out, attns = self.encoder(enc_out)
+        # z: [bs x nvars x patch_num x d_model]
+        enc_out = torch.reshape(
+            enc_out, (-1, n_vars, enc_out.shape[-2], enc_out.shape[-1])
+        )
+        # z: [bs x nvars x d_model x patch_num]
+        enc_out = enc_out.permute(0, 1, 3, 2)
+
+        # Decoder
+        output = self.flatten(enc_out)
+        output = self.dropout(output)
+        output = output.reshape(output.shape[0], -1)
+        output = self.projection(output)  # (batch_size, num_classes)
+        return output
+
+    def semantic_segmentation(self, x_enc):
+        # Normalization from Non-stationary Transformer
+        means = x_enc.mean(1, keepdim=True).detach()
+        x_enc = x_enc - means
+        stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+        x_enc /= stdev
+
+        # do patching and embedding
+        x_enc = x_enc.permute(0, 2, 1)
+        # u: [bs * nvars x patch_num x d_model]
+        enc_out, n_vars = self.patch_embedding(x_enc)
+
+        # Encoder
+        # z: [bs * nvars x patch_num x d_model]
+        enc_out, attns = self.encoder(enc_out)
+        # z: [bs x nvars x patch_num x d_model]
+        enc_out = torch.reshape(
+            enc_out, (-1, n_vars, enc_out.shape[-2], enc_out.shape[-1])
+        )
+        # z: [bs x nvars x d_model x patch_num]
+        enc_out = enc_out.permute(0, 1, 3, 2)
+
+        # Decoder
+        dec_out = enc_out.reshape(enc_out.shape[0], -1)
+        dec_out = self.projection(dec_out)
+
+        if not self.training:
+            if self.num_class > 2:
+                dec_out = dec_out.reshape(
+                    dec_out.shape[0], self.pred_len, self.num_class
+                )
+                dec_out = F.softmax(dec_out, dim=-1)
+            else:
+                dec_out = F.sigmoid(dec_out)
+
+        return dec_out
+
+    def segmentation(self, x_enc):
+        # Normalization from Non-stationary Transformer
+        means = x_enc.mean(1, keepdim=True).detach()
+        x_enc = x_enc - means
+        stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
+        x_enc /= stdev
+
+        # do patching and embedding
+        x_enc = x_enc.permute(0, 2, 1)
+        # u: [bs * nvars x patch_num x d_model]
+        enc_out, n_vars = self.patch_embedding(x_enc)
+
+        # Encoder
+        # z: [bs * nvars x patch_num x d_model]
+        enc_out, attns = self.encoder(enc_out)
+        # z: [bs x nvars x patch_num x d_model]
+        enc_out = torch.reshape(
+            enc_out, (-1, n_vars, enc_out.shape[-2], enc_out.shape[-1])
+        )
+        # z: [bs x nvars x d_model x patch_num]
+        enc_out = enc_out.permute(0, 1, 3, 2)
+
+        # Decoder
+        dec_out = enc_out.reshape(enc_out.shape[0], -1)
+        dec_out = self.projection(dec_out)
+
+        if not self.training:
+            dec_out = F.sigmoid(dec_out)
+
+        return dec_out
+
+    def forward(self, inputs):
+        x_enc = inputs["x_enc"]
+        x_dec = inputs.get("x_dec", None)
+        x_mark_enc = inputs.get("x_mark_enc", None)
+        x_mark_dec = inputs.get("x_mark_dec", None)
+        mask = inputs.get("mask", None)
+
+        if self.task_name == "forecasting":
+            dec_out = self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
+            return dec_out[:, -self.pred_len :, :]  # [B, L, D]
+        if self.task_name == "imputation":
+            dec_out = self.imputation(x_enc, x_mark_enc, x_dec, x_mark_dec, mask)
+            return dec_out  # [B, L, D]
+        if self.task_name == "anomaly_detection":
+            dec_out = self.anomaly_detection(x_enc)
+            return dec_out  # [B, L, D]
+        if self.task_name == "classification":
+            dec_out = self.classification(x_enc, x_mark_enc)
+            return dec_out  # [B, N]
+        if self.task_name == "semantic_segmentation":
+            dec_out = self.semantic_segmentation(x_enc)
+            return dec_out
+        if self.task_name == "segmentation":
+            dec_out = self.segmentation(x_enc)
+            return dec_out
+        return None
+
+
+class FlattenHead(nn.Module):
+    def __init__(self, n_vars, nf, target_window, head_dropout=0):
+        super().__init__()
+        self.n_vars = n_vars
+        self.flatten = nn.Flatten(start_dim=-2)
+        self.linear = nn.Linear(nf, target_window)
+        self.dropout = nn.Dropout(head_dropout)
+
+    def forward(self, x):  # x: [bs x nvars x d_model x patch_num]
+        x = self.flatten(x)
+        x = self.linear(x)
+        x = self.dropout(x)
+        return x
+
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, d_model, patch_len, stride, padding, dropout):
+        super().__init__()
+        # Patching
+        self.patch_len = patch_len
+        self.stride = stride
+        self.padding_patch_layer = nn.ReplicationPad1d((0, padding))
+
+        # Backbone, Input encoding: projection of feature vectors onto a d-dim vector space
+        self.value_embedding = nn.Linear(patch_len, d_model, bias=False)
+
+        # Positional embedding
+        self.position_embedding = PositionalEmbedding(d_model)
+
+        # Residual dropout
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # do patching
+        n_vars = x.shape[1]
+        x = self.padding_patch_layer(x)
+        x = x.unfold(dimension=-1, size=self.patch_len, step=self.stride)
+        x = torch.reshape(x, (x.shape[0] * x.shape[1], x.shape[2], x.shape[3]))
+        # Input encoding
+        x = self.value_embedding(x) + self.position_embedding(x)
+        return self.dropout(x), n_vars
--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@ from .gpt4ts import GPT4TS
 
 from .dlinear import DLinear
 from .FEDformer import FEDformer
+from .PatchTST import PatchTST
 
 
 model_lookup = {
@@ -10,4 +11,5 @@ model_lookup = {
 	"gpt4ts": GPT4TS,
     "dlinear": DLinear,
     "fedformer": FEDformer,
+    "patchtst": PatchTST,
 }
