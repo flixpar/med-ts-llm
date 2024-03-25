1d35f2701216ac951734fe0e6226f9c8a9ed454a
Felix Parker <flixpar@gmail.com>
2024-03-25 00:41:16 -0400

    minor fix

--- loggers/wandb_logger.py
+++ loggers/wandb_logger.py
@@ -23,10 +23,6 @@ class WandBLogger(BaseLogger):
 
     def log_end(self):
         self.logger.log_code()
-
-        metrics = self.logger.history()
-        metrics.to_csv(self.logdir / "metrics.csv")
-
         self.logger.finish()
 
     def log_scores(self, scores={}, **kwscores):
