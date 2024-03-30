1c9e6745f0fe3ac92ebe497904e38f2e7808b544
Felix Parker <flixpar@gmail.com>
2024-03-30 01:19:06 -0400

    add debug logger

--- loggers/__init__.py
+++ loggers/__init__.py
@@ -1,12 +1,17 @@
 from .wandb_logger import WandBLogger
 from .tensorboard_logger import TensorboardLogger
+from .print_logger import PrintLogger
 
 
 def get_logger(trainer, config, newrun=True):
+    if config.DEBUG:
+        return PrintLogger(trainer, config, newrun)
     match config.setup.logger:
         case "wandb":
             return WandBLogger(trainer, config, newrun)
         case "tensorboard":
             return TensorboardLogger(trainer, config, newrun)
+        case "print" | "none":
+            return PrintLogger(trainer, config, newrun)
         case _:
             raise ValueError(f"Unknown logger: {config.setup.logger}")
---
+++ loggers/print_logger.py
@@ -0,0 +1,26 @@
+import json
+
+from .base_logger import BaseLogger
+from utils import summarize_config
+
+
+class PrintLogger(BaseLogger):
+    def __init__(self, trainer, config, newrun=True):
+        config = summarize_config(config)
+        config = json.dumps(config, indent="\t")
+
+        print("Run ID:", trainer.run_id)
+        print("Config:")
+        print(config)
+
+    def log_end(self):
+        print("Done!")
+
+    def log_scores(self, scores={}, **kwscores):
+        scores = scores | kwscores
+        if len(scores) == 1 and "train_loss" in scores:
+            return
+        print(f"Epoch: {self.trainer.epoch}, step: {self.trainer.step}, scores: {scores}")
+
+    def save_state(self, name):
+        pass
