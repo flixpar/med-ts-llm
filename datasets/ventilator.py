2d0ffb75f354ec6008231dfd6b7b2d372278e1ed
Felix Parker <flixpar@gmail.com>
2024-04-05 18:13:58 -0400

    overhaul ventilator semseg dataset

--- datasets/ventilator.py
+++ datasets/ventilator.py
@@ -1,6 +1,7 @@
 from abc import ABC
 from pathlib import Path
 import pandas as pd
+import re
 
 from .base import BaseDataset, ForecastDataset, ReconstructionDataset, SemanticSegmentationDataset
 
@@ -74,6 +75,21 @@ class VentilatorReconstructionDataset(VentilatorDataset, ReconstructionDataset):
 
 class VentilatorSemanticSegmentationDataset(VentilatorDataset, SemanticSegmentationDataset):
 
+    train_clips = [
+        "patient_572_vent_w_1_labeled",
+        "patient_572_vent_w_2_labeled",
+        "patient_572_vent_w_4_labeled", # async
+        "patient_674_vent_w_1_labeled",
+        "patient_674_vent_w_4_labeled",
+        "patient_697_vent_w_1_labeled",
+        "patient_697_vent_w_2_labeled",
+    ]
+    test_clips = [
+        "patient_704_vent_w_1_labeled", # anom
+        "patient_709_vent_w_1_labeled",
+        "patient_709_vent_w_2_labeled", # async
+    ]
+
     @property
     def n_classes(self):
         return 2
@@ -81,35 +97,36 @@ class VentilatorSemanticSegmentationDataset(VentilatorDataset, SemanticSegmentat
     def get_data(self, split=None):
         split = split or self.split
 
-        basepath = Path(__file__).parent / "../data/ventilator/v3/"
-        waveform_files = sorted(basepath.glob("*.csv"))
+        assert self.dataset_config.version == "v4"
+        assert self.dataset_config.split_version == "v1"
 
-        dfs = [pd.read_csv(f, usecols=["pressure", "flow", "label"]) for f in waveform_files]
-        dfs = [df[df.label >= 0] for df in dfs]
-        dfs = [df for df in dfs if len(df) > 1000]
+        basepath = Path(__file__).parent / "../data/ventilator/v4/"
+        clip_list = self.train_clips if split == "train" else self.test_clips
+
+        dfs = []
+        for clip_id in clip_list:
+            fn = basepath / f"{clip_id}.csv"
+            df = pd.read_csv(fn)
+            df = df[df.label >= 0]
+            df["clip_id"] = parse_clip_id(clip_id)
+            dfs.append(df)
         data = pd.concat(dfs, ignore_index=True)
 
+        features = data[["pressure", "flow"]].values
         labels = data["label"].values.astype(int)
-        data = data[["pressure", "flow"]].values
+        clip_ids = data["clip_id"].values
+        timestamps = data["dt"].values.astype(float)
 
-        train_pct, val_pct, test_pct = 0.7, 0.15, 0.15
-        train_idx = int(train_pct * data.shape[0])
-        val_idx = int((train_pct + val_pct) * data.shape[0])
+        return {"data": features, "labels": labels, "clip_ids": clip_ids, "timestamps": timestamps}
 
-        match split:
-            case "train":
-                data = data[:train_idx,:]
-                labels = labels[:train_idx]
-            case "val":
-                data = data[train_idx:val_idx,:]
-                labels = labels[train_idx:val_idx]
-            case "test":
-                data = data[val_idx:,:]
-                labels = labels[val_idx:]
-            case _:
-                raise ValueError(f"Invalid split: {split}")
 
-        return {"data": data, "labels": labels}
+def parse_clip_id(clip_string):
+    match = re.match(r"patient_(\d+)_vent_w_(\d+)", clip_string)
+    patient_id, clip_number = match.groups()
+    patient_id, clip_number = int(patient_id), int(clip_number)
+    clip_id = (patient_id * 100) + clip_number
+    print(f"Clip string: {clip_string}, Clip ID: {clip_id}")
+    return clip_id
 
 
 ventilator_datasets = {
