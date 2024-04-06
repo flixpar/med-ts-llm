374a3cf905b853332ebd19bb66bb73d587935ce2
Felix Parker <flixpar@gmail.com>
2024-04-05 20:28:53 -0400

    add anomaly detection for ventilator dataset

--- datasets/ventilator.py
+++ datasets/ventilator.py
@@ -3,7 +3,13 @@ from pathlib import Path
 import pandas as pd
 import re
 
-from .base import BaseDataset, ForecastDataset, ReconstructionDataset, SemanticSegmentationDataset
+from .base import (
+    BaseDataset,
+    ForecastDataset,
+    ReconstructionDataset,
+    SemanticSegmentationDataset,
+    AnomalyDetectionDataset,
+)
 
 
 class VentilatorDataset(BaseDataset, ABC):
@@ -12,6 +18,7 @@ class VentilatorDataset(BaseDataset, ABC):
         "forecasting",
         "reconstruction",
         "semantic_segmentation",
+        "anomaly_detection",
     ]
 
     description = "The dataset contains time-series data of airway pressure and flow rate measurements collected from a mechanical ventilator during the respiratory support of a fully sedated patient. The data is sampled at a frequency of 100 Hz. The airway pressure is measured in cmH2O and the flow rate is measured in L/min."
@@ -120,12 +127,56 @@ class VentilatorSemanticSegmentationDataset(VentilatorDataset, SemanticSegmentat
         return {"data": features, "labels": labels, "clip_ids": clip_ids, "timestamps": timestamps}
 
 
+class VentilatorAnomalyDetectionDataset(VentilatorDataset, AnomalyDetectionDataset):
+
+    train_clips = [
+        "patient_572_vent_w_1_labeled",
+        "patient_572_vent_w_2_labeled",
+        "patient_572_vent_w_4_labeled", # async
+        "patient_674_vent_w_1_labeled",
+        "patient_674_vent_w_4_labeled",
+        "patient_697_vent_w_1_labeled",
+        "patient_697_vent_w_2_labeled",
+        "patient_709_vent_w_1_labeled",
+        "patient_709_vent_w_2_labeled", # async
+    ]
+    test_clips = [
+        "patient_704_vent_w_1_labeled", # anom
+    ]
+
+    def get_data(self, split=None):
+        split = split or self.split
+
+        assert self.dataset_config.version == "v4"
+        assert self.dataset_config.split_version == "v1"
+
+        basepath = Path(__file__).parent / "../data/ventilator/v4/"
+        clip_list = self.train_clips if split == "train" else self.test_clips
+
+        dfs = []
+        for clip_id in clip_list:
+            fn = basepath / f"{clip_id}.csv"
+            df = pd.read_csv(fn)
+            if split == "train":
+                df = df[df.label >= 0]
+            df["label"] = df["label"] < 0
+            df["clip_id"] = parse_clip_id(clip_id)
+            dfs.append(df)
+        data = pd.concat(dfs, ignore_index=True)
+
+        features = data[["pressure", "flow"]].values
+        labels = data["label"].values.astype(int)
+        clip_ids = data["clip_id"].values
+        timestamps = data["dt"].values.astype(float)
+
+        return {"data": features, "labels": labels, "clip_ids": clip_ids, "timestamps": timestamps}
+
+
 def parse_clip_id(clip_string):
     match = re.match(r"patient_(\d+)_vent_w_(\d+)", clip_string)
     patient_id, clip_number = match.groups()
     patient_id, clip_number = int(patient_id), int(clip_number)
     clip_id = (patient_id * 100) + clip_number
-    print(f"Clip string: {clip_string}, Clip ID: {clip_id}")
     return clip_id
 
 
@@ -133,4 +184,5 @@ ventilator_datasets = {
     "forecasting": VentilatorForecastingDataset,
     "reconstruction": VentilatorReconstructionDataset,
     "semantic_segmentation": VentilatorSemanticSegmentationDataset,
+    "anomaly_detection": VentilatorAnomalyDetectionDataset,
 }
